ca9dc4b12ec4d34d0bb428f4cb7f3e8921deef40
mikhailov <asdgrghd@gmail.com>
2025-06-06 21:20:44 +0400

    Added datetime field in SQLModel

--- src/api/events/model.py
+++ src/api/events/model.py
@@ -1,12 +1,22 @@
 from typing import List, Optional
-#from pydantic import BaseModel, Field
+import sqlmodel
+from datetime import datetime, timezone
 from sqlmodel import SQLModel, Field
+
+
+def get_utc_now():
+    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
 
 
 class EventModel(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     page: Optional[str] = ""
     description: Optional[str] = ""
+    created_at: datetime = Field(
+        default_factory=get_utc_now,
+        sa_type=sqlmodel.DateTime(timezone=True),
+        nullable=False
+    )
 
 
 class EventListSchema(SQLModel):
